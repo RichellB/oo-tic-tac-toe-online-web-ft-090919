f6cb62412bf0de5b971301eb78c2ce68c4b02ee7
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-23 01:42:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -87,11 +87,11 @@ end
   def draw?(board)
     if !won(board) && full?(board)
       return true 
-  elsif !won?(board) && !full?(board)
-    return false 
-  else won?(board)
-    return false 
-  end 
-end
+    elsif !won?(board) && !full?(board)
+      return false 
+    else won?(board)
+      return false 
+    end 
+  end
 
 end
